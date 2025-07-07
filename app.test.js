76be07956756b990842b35e0b4030e11c76bbd93
Stu Watts <stu.watts@gmail.com>
2025-07-07 21:13:36 +0100

    add a multiply function and unit test

--- app.test.js
+++ app.test.js
@@ -1,5 +1,10 @@
 const sum = require("./app").sum;
+const multiply = require("./app").multiply;
 
 test("add two numbers", () => {
   expect(sum(3, 5)).toBe(8);
+});
+
+test("multiply two numbers", () => {
+  expect(multiply(3, 5)).toBe(15);
 });
